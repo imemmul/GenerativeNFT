ca12296ad19f145026ff296037c8824fed620152
imemmul <emrulurak17@outlook.com>
2023-10-13 23:24:23 +0300

    downloading images

--- dataset/download_dataset.py
+++ dataset/download_dataset.py
@@ -11,33 +11,44 @@ def download_dataset(args):
     downloading dataset
     """
     for cat_dir in os.listdir(args.json_dir):
-         cat_nft = os.path.join(args.json_dir, cat_dir)
-         for j in os.listdir(cat_nft):
-            j_dir = os.path.join(cat_nft, j)
-            # download jsons to output
-            with open(j_dir, "r") as json_file:
-                data = json.load(json_file)
-            image_url = data.get("image_url") # changeable
-            nft_name = data.get("nft_name") # changeable
-            filename = j[:-4] + 'jpg'
-            download_dir = os.path.join(args.output_dir, cat_dir)
-            os.makedirs(download_dir, exist_ok=True)
-            response = requests.get(image_url)
-            if args.download:
-                if response.status_code == 200:
-                    with open(os.path.join(download_dir, filename), 'wb') as img_file: # images will be change
-                        img_file.write(response.content)
-                    print(f'Saved {filename}')
-                else:
-                    print(f'Failed to download image')
-            if args.verbose:
-                print(f"Downloading: {nft_name}")
+        cat_nft = os.path.join(args.json_dir, cat_dir)
+        if cat_dir == "Portals":
+            pass
+        else:
+            for j in os.listdir(cat_nft):
+                j_dir = os.path.join(cat_nft, j)
+                try:
+                    with open(j_dir, "r", encoding='cp1252') as json_file:
+                        data = json.load(json_file)
+                except UnicodeDecodeError as e:
+                    print(f"changing decoding")
+                    with open(j_dir, "r", encoding='utf-8') as json_file:
+                        data = json.load(json_file)
+                if data:
+                    nft_name = data[0]["mintObject"]["title"]
+                    image_url = data[0]["mintObject"]["img"]
+                    print(f"Downloading: {nft_name} with {image_url}")
+                    if args.download:
+                        filename = j[:-4] + 'png'
+                        download_dir = os.path.join(args.output_dir, cat_dir)
+                        os.makedirs(download_dir, exist_ok=True)
+                        response = requests.get(image_url)
+                        if response.status_code == 200:
+                            with open(os.path.join(download_dir, filename), 'wb') as img_file:
+                                img_file.write(response.content)
+                            print(f'Saved to {download_dir}{filename}')
+                            os.remove(j_dir)
+                        else:
+                            print(f'Failed to download image')
+                        if args.verbose:
+                            print(f"Downloading: {nft_name}")
+                        
 
 
 def parseargs():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--json_dir", help="Json dataaset directory")
-    parser.add_argument("--output_dir", help="Download directory")
+    parser.add_argument("--json-dir", help="Json dataaset directory")
+    parser.add_argument("--output-dir", help="Download directory")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose mode")
     parser.add_argument("--download", action="store_true", help="test or download")
     args = parser.parse_args()
