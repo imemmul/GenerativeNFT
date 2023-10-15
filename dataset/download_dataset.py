7fc277db243dd7648f4712e82b9eb7d97e561b9e
imemmul <emrulurak17@outlook.com>
2023-10-15 09:49:41 +0300

    downloading image debug

--- dataset/download_dataset.py
+++ dataset/download_dataset.py
@@ -4,46 +4,60 @@ import json
 import requests
 from urllib.parse import urlparse
 
-
-
+def load_json(json_dir):
+    data = None
+    try:
+        with open(json_dir, "r", encoding='cp1252') as json_file:
+            data = json.load(json_file)
+    except UnicodeDecodeError as e:
+        print(f"changing decoding")
+        with open(json_dir, "r", encoding='utf-8') as json_file:
+            data = json.load(json_file)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        return data
 def download_dataset(args):
     """
     downloading dataset
     """
     for cat_dir in os.listdir(args.json_dir):
         cat_nft = os.path.join(args.json_dir, cat_dir)
-        if cat_dir == "Portals":
+        if cat_dir == "Portals" or cat_dir == "Genesis Genopet":
             pass
         else:
+            amount = 0
+            if os.path.exists(os.path.join(args.output_dir, cat_dir)):
+                amount = len(os.listdir(os.path.join(args.output_dir, cat_dir)))
             for j in os.listdir(cat_nft):
                 j_dir = os.path.join(cat_nft, j)
-                try:
-                    with open(j_dir, "r", encoding='cp1252') as json_file:
-                        data = json.load(json_file)
-                except UnicodeDecodeError as e:
-                    print(f"changing decoding")
-                    with open(j_dir, "r", encoding='utf-8') as json_file:
-                        data = json.load(json_file)
+                if args.verbose:
+                    print(f"Reading: {j_dir}")
+                data = load_json(j_dir)
                 if data:
                     nft_name = data[0]["mintObject"]["title"]
                     image_url = data[0]["mintObject"]["img"]
-                    print(f"Downloading: {nft_name} with {image_url}")
+                    if args.verbose:
+                        print(f"Downloading: {nft_name} with {image_url}")
                     if args.download:
                         filename = j[:-4] + 'png'
                         download_dir = os.path.join(args.output_dir, cat_dir)
                         os.makedirs(download_dir, exist_ok=True)
-                        response = requests.get(image_url)
-                        if len(os.listdir(download_dir)) <= 3000:
-                            if response.status_code == 200:
-                                with open(os.path.join(download_dir, filename), 'wb') as img_file:
-                                    img_file.write(response.content)
-                                print(f'Saved to {download_dir}{filename}')
+                        if amount <= 3000:
+                            if os.path.exists(os.path.join(download_dir, filename)):
+                                print(f"Already exists: {nft_name}")
+                                continue
                             else:
-                                print(f'Failed to download image')
-                            if args.verbose:
-                                print(f"Downloading: {nft_name}")
+                                response = requests.get(image_url)
+                                if response.status_code == 200:
+                                    with open(os.path.join(download_dir, filename), 'wb') as img_file:
+                                        img_file.write(response.content)
+                                    amount += 1
+                                    print(f'Saved to {download_dir}{filename} as {amount}th image')
+                                else:
+                                    print(f'Failed to download image')
                         else:
-                            break
+                            pass
                         
 
 
