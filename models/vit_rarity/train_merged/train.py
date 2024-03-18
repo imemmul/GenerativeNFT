75f80cbe28369aeeb3a81487e842f83df0172ad1
imemmul <emrulurak17@outlook.com>
2024-03-18 21:03:18 +0300

    trained

--- models/vit_rarity/train_merged/train.py
+++ models/vit_rarity/train_merged/train.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import unicodedata
 import torch.optim as optim
 from torch.utils.data import random_split, DataLoader
 import ast
@@ -13,6 +14,25 @@ from torchvision import datasets, transforms
 from transformers import ViTForImageClassification
 import pandas as pd
 import argparse
+from sklearn.metrics import accuracy_score, f1_score, precision_score, confusion_matrix
+import torch
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def fix_dir(image_dir):
+    """
+    this function takes a image dir and fixs it so that it exist in dataset
+    """
+    norms = ['NFC', 'NFD', 'NFKC', 'NFKD']
+    if not os.path.isfile(image_dir):
+        for norm in norms:
+            print(f"norming: {norm}")
+            img_dir_normalized = unicodedata.normalize(norm, image_dir)
+            img_dir_normalized = img_dir_normalized.replace("'", "_")
+            if os.path.isfile(img_dir_normalized):
+                return img_dir_normalized
+    return image_dir
+
 
 def extract_rank(row):
     return row['rank'] if row and 'rank' in row else None
@@ -27,7 +47,17 @@ def convert_to_dict(string_repr):
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Resize((224, 224)),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomErasing(),
+    transforms.ColorJitter()
+])
+
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((224, 224)),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 
@@ -42,35 +72,27 @@ class RarityDatasetMerged(Dataset):
     def __len__(self):
         return len(self.labels_old) + len(self.labels_new)
 
+
     def __getitem__(self, index):
-        if index >= len(self.labels_old):
-            index = index - len(self.labels_old)
-            if "augmented" in self.labels_new.iloc[index].data_name:
+        try:
+            if index >= len(self.labels_old):
+                index = index - len(self.labels_old)
                 img_dir = os.path.join(self.images_dir_new, self.labels_new.iloc[index].data_name)
-            else:
-                img_dir = os.path.join(self.images_dir_new, self.labels_new.iloc[index].data_name + ".png")
-            try:
-                img = np.array(Image.open(img_dir).convert('RGB'))
-                print(self.labels_new.iloc[index].data_name)
+                img_dir_fixed = fix_dir(img_dir)
+                img = np.array(Image.open(img_dir_fixed).convert('RGB'))
                 if self.transform:
                     img = self.transform(img)
                 return img, self.labels_new.iloc[index].cls
-            except Exception as e:
-                pass
-                
-        else:
-            if "augmented" in self.labels_old.iloc[index].onChainName:
-                img_dir = os.path.join(self.images_dir_old, self.labels_old.iloc[index].onChainName)
             else:
-                img_dir = os.path.join(self.images_dir_old, self.labels_old.iloc[index].onChainName + ".png")
-            try:
-                img = np.array(Image.open(img_dir).convert('RGB'))
-                print(self.labels_old.iloc[index].onChainName)
+                img_dir = os.path.join(self.images_dir_old, self.labels_old.iloc[index].data_name + ".png")
+                img_dir_fixed = fix_dir(img_dir)
+                img = np.array(Image.open(img_dir_fixed).convert('RGB'))
                 if self.transform:
                     img = self.transform(img)
                 return img, self.labels_old.iloc[index].cls_label
-            except Exception as e:
-                pass
+                    
+        except Exception as e:
+            raise RuntimeError(f"Error loading image at index {index}: {str(e)}")
 
 
 def _arg_parse():
@@ -82,6 +104,8 @@ def _arg_parse():
     args.add_argument("--label_dir_old", type=str)
     args.add_argument("--label_dir_new", type=str)
     args.add_argument("--num_epochs", type=int)
+    args.add_argument("--train", action="store_true")
+    args.add_argument("--checkpoint", type=str)
     return args.parse_args()
 
 def val(model, test_loader, device, criterion):
@@ -102,45 +126,92 @@ def val(model, test_loader, device, criterion):
 
 def _train():
     args = _arg_parse()
-    dataset = RarityDatasetMerged(label_dir_old=args.label_dir_old, label_dir_new=args.label_dir_new,
-                                  image_dir_old=args.images_dir_old, image_dir_new=args.images_dir_new, transform=transform)
+    if args.train:
+        dataset = RarityDatasetMerged(label_dir_old=args.label_dir_old, label_dir_new=args.label_dir_new,
+                                      image_dir_old=args.images_dir_old, image_dir_new=args.images_dir_new, transform=transform)
+    else:
+        dataset = RarityDatasetMerged(label_dir_old=args.label_dir_old, label_dir_new=args.label_dir_new,
+                                      image_dir_old=args.images_dir_old, image_dir_new=args.images_dir_new, transform=test_transform)
 
     train_dataset, val_dataset = random_split(dataset, [int(len(dataset)*args.split_ratio), int(len(dataset) - int(len(dataset)*args.split_ratio))])
     print(len(train_dataset))
     print(len(val_dataset))
-    
-    print("Training set size:", len(train_dataset))
-    print("Val set size:", len(val_dataset))
-    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False)
+    if args.train:
+        print("Training set size:", len(train_dataset))
+        train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False)
+        print("Val set size:", len(val_dataset))
+    else:
+        print("Test set size:", len(val_dataset))
+        val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False)
     model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224-in21k')
-    model.classifier = nn.Linear(model.config.hidden_size, 1)
+    model.classifier = nn.Linear(model.config.hidden_size, 1) # TODO
     optimizer = optim.Adam(model.parameters(), lr=1e-4)
     criterion = nn.BCELoss()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     model.train()
-    for epoch in range(args.num_epochs):
-        train_loss = 0
-        for i, batch in enumerate(train_loader):
-            images, labels = batch
-            images, labels = images.to(device), labels.to(device)
+    if args.train:
+        for epoch in range(args.num_epochs):
+            train_loss = 0
+            for i, batch in enumerate(train_loader):
+                images, labels = batch
+                images, labels = images.to(device), labels.to(device)
+                
+                optimizer.zero_grad()
+                outputs = model(images)
+                labels = labels.unsqueeze(1).to(outputs.logits.dtype)
+
+                probs = torch.sigmoid(outputs.logits)
+                # print(f"probs: {probs}, shape: {probs.shape}")
+                # print(f"labels: {labels}, shape: {labels.shape}")
+                loss = criterion(probs, labels)
+                loss.backward()
+                optimizer.step()
+                print(f"iteration {i}, loss: {loss.item()}")
+                train_loss += loss.item()
+            val_loss = val(model, val_loader, device, criterion=criterion)
+            print(f"Epoch [{epoch+1}/{args.num_epochs}], Train Loss: {train_loss / len(train_loader)} val_loss: {val_loss}")
+        torch.save(model.state_dict(), "/home/emir/Desktop/dev/datasets/weights/run_01_merged_clsf.pt")
+    else:
+        if args.checkpoint:
+            model.to("cuda")
+            model.load_state_dict(torch.load(args.checkpoint))
+            model.eval()
+
+            true_labels = []
+            predicted_labels = []
+            print("========================================")
+            print("TESTING")
+            print("========================================")
+            with torch.no_grad():
+                for i, batch in enumerate(val_loader):
+                    img, label = batch
+                    output = model(img.to("cuda"))
+
+                    true_labels.extend(label.numpy())
+                    predicted_labels.extend((torch.sigmoid(output.logits).detach().cpu().numpy() > 0.5).astype(int))
+
+            true_labels = np.array(true_labels)
+            predicted_labels = np.array(predicted_labels)
+
+            acc = accuracy_score(true_labels, predicted_labels)
+            f1 = f1_score(true_labels, predicted_labels)
+            p_score = precision_score(true_labels, predicted_labels)
+
+            conf_matrix = confusion_matrix(true_labels, predicted_labels)
+
+            plt.figure(figsize=(8, 6))
+            sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=['Predicted 0', 'Predicted 1'], yticklabels=['Actual 0', 'Actual 1'])
+            plt.xlabel('Predicted Label')
+            plt.ylabel('True Label')
+            plt.title('Confusion Matrix')
+            plt.show()
+            plt.savefig("./conf.png")
+
+            print(f'Accuracy: {acc}')
+            print(f'F1 Score: {f1}')
+            print(f'Precision Score: {p_score}')
             
-            optimizer.zero_grad()
-            outputs = model(images)
-            labels = labels.unsqueeze(1).to(outputs.logits.dtype)
-
-            probs = torch.sigmoid(outputs.logits)
-            # print(f"probs: {probs}, shape: {probs.shape}")
-            # print(f"labels: {labels}, shape: {labels.shape}")
-            loss = criterion(probs, labels)
-            loss.backward()
-            optimizer.step()
-            print(f"iteration {i}, loss: {loss.item()}")
-            train_loss += loss.item()
-        val_loss = val(model, val_loader, device, criterion=criterion)
-        print(f"Epoch [{epoch+1}/{args.num_epochs}], Train Loss: {train_loss / len(train_loader)} val_loss: {val_loss}")
-    torch.save(model.state_dict(), "/home/emir/Desktop/dev/datasets/nft_rarity_dataset/weights/run_02_merged_clsf.pt")
-
 if __name__ == "__main__":
     _train()
