4b64f6093854fa7adf3911606f9aab411fbce4e8
Beyza Kaya <beyzakaya@192.168.1.8>
2024-04-28 18:47:00 +0300

    prediction count

--- models/vit_rarity/evaluate_base_vit.py
+++ models/vit_rarity/evaluate_base_vit.py
@@ -17,6 +17,7 @@ class CustomDataset(Dataset):
     
     def __getitem__(self,idx):
         img_name = self.label_df['data_name']
+        print(f"Image name: {img_name}")
         image = Image.open(img_name).convert('RGB')
         label = self.label_df[idx]['cls']
 
@@ -29,6 +30,9 @@ class CustomDataset(Dataset):
 # 0 for not rare
 def evaluate_dataset(model,dataloader,device):
     model.eval()
+    correct_predictions = 0
+    false_predictions = 0
+    total_predictions = 0
     with torch.no_grad():
         for images,labels in dataloader:
             images = images.to(device)
@@ -38,8 +42,18 @@ def evaluate_dataset(model,dataloader,device):
             predictions = torch.sigmoid(outputs.logits)
 
             for prediction, label in zip(predictions, labels):
-
+                total_predictions += 1
+                if (prediction >= 0.5 and label == 1) or (prediction < 0.5 and label == 0):
+                    correct_predictions += 1
+                if(prediction >= 0.5 and label != 1) or (prediction < 0.5 and label != 0):
+                    false_predictions += 1
                 if prediction >= 0.5:
                     print(f"Image is predicted as RARE with confidence: {prediction.item()}, Actual label: {label}")
                 else:
                     print(f"Image is predicted as NOT RARE with confidence: {1 - prediction.item()}, Actual label: {label}")
+    
+    print(print(f"Total predictions: {total_predictions}, Correct predictions: {correct_predictions}"))
+
+
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224-in21k')
+
