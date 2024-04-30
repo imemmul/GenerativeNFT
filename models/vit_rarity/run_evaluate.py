fa7ea87d92dd5cf6499b49c431e96bdfd51297e4
Beyza Kaya <beyzakaya@192.168.1.8>
2024-04-30 15:13:19 +0300

    testing

--- models/vit_rarity/run_evaluate.py
+++ models/vit_rarity/run_evaluate.py
@@ -2,20 +2,25 @@ import argparse
 from evaluate_base_vit import CustomDataset, ViTModelEvaluator
 from torch.utils.data import DataLoader
 from torchvision import transforms
+import random
 
 def argument_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_dir", type=str, help="Path to directory containing images")
     parser.add_argument("--label_dir", type=str, help="Path to CSV file containing labels")
     parser.add_argument("--vit_model_weights_path", type=str, help="Path to ViT model weights file")
+    parser.add_argument("--subset_size", type=int, default=None, help="Number of samples for the subset (default: None, which means using the entire dataset)")
     return parser.parse_args()  
 
-def main(image_dir, label_dir, vit_model_weights_path):
+def main(image_dir, label_dir, vit_model_weights_path, subset_size=None):
     custom_dataset = CustomDataset(image_dir, label_dir)
+    if subset_size is not None:
+        indices = random.sample(range(len(custom_dataset)), min(subset_size, len(custom_dataset)))
+        custom_dataset = torch.utils.data.Subset(custom_dataset, indices)
     dataloader = DataLoader(custom_dataset, batch_size=8)
     vit_evaluator = ViTModelEvaluator(vit_model_weights_path)
     vit_evaluator.evaluate_dataset(dataloader)
 
 if __name__ == "__main__":
     args = argument_parse()
-    main(args.image_dir, args.label_dir, args.vit_model_weights_path)
+    main(args.image_dir, args.label_dir, args.vit_model_weights_path, args.subset_size)
